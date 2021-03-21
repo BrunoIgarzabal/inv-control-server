97232daae74b7009c5faba2aabe28965d8fd1a9c
Bruno Igarzabal <brunoigar@live.com>
2021-03-21 14:56:21 -0300

    chore: add h2, spring web, jpa

---
+++ src/main/java/com/brunoIgarzabal/invcontrol/InvControlApplication.java
@@ -0,0 +1,13 @@
+package com.brunoIgarzabal.invcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunoIgarzabal/invcontrol/InvControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoIgarzabal.invcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
